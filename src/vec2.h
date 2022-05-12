ca631bcd4db86d85c5278b99c671fbd034091247
euanmills <euanmills26@gmail.com>
2022-05-12 23:01:41 +0100

    Add more vector2 operators

--- src/vec2.h
+++ src/vec2.h
@@ -10,11 +10,29 @@ public:
     Vector2(const float &x, const float &y);
 
     friend const Vector2 operator+(const Vector2 &left, const Vector2 &right);
+    friend const Vector2 operator-(const Vector2 &left, const Vector2 &right);
+    friend const Vector2 operator*(const Vector2 &left, const Vector2 &right);
+    friend const Vector2 operator*(const Vector2 &left, const float &scale);
 };
 
 inline const Vector2 operator+(const Vector2 &left, const Vector2 &right)
 {
     return Vector2(left.x + right.x, left.y + right.y);
+}
+
+inline const Vector2 operator-(const Vector2 &left, const Vector2 &right)
+{
+    return Vector2(left.x - right.x, left.y - right.y);
+}
+
+inline const Vector2 operator*(const Vector2 &left, const Vector2 &right)
+{
+    return Vector2(left.x * right.x, left.y * right.y);
+}
+
+inline const Vector2 operator*(const Vector2 &left, const float &scale)
+{
+    return Vector2(left.x * scale, left.y * scale);
 }
 
 #endif
