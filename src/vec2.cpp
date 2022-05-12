96286c776f8d0ac2897a021267d7fc6ee70dc2ff
euanmills <euanmills26@gmail.com>
2022-05-12 21:33:04 +0100

    Implement addition of vector2s

---
+++ src/vec2.cpp
@@ -0,0 +1,5 @@
+#include "vec2.h"
+
+Vector2::Vector2(): x(0.0f), y(0.0f) { }
+
+Vector2::Vector2(const float &x, const float &y) { }
--- src/vec2.h
+++ src/vec2.h
@@ -8,6 +8,13 @@ public:
 
     Vector2();
     Vector2(const float &x, const float &y);
+
+    friend const Vector2 operator+(const Vector2 &left, const Vector2 &right);
 };
+
+inline const Vector2 operator+(const Vector2 &left, const Vector2 &right)
+{
+    return Vector2(left.x + right.x, left.y + right.y);
+}
 
 #endif
